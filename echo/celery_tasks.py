08a6ba9712f48d4263da7072a79992462cd74699
Mark Burnett <mark.m.burnett@gmail.com>
2014-12-07 20:27:26 -0600

    rely on taskx_service implementation of webhook

--- echo/celery_tasks.py
+++ echo/celery_tasks.py
@@ -1,26 +1,11 @@
 from . import models
-from celery import shared_task
-from celery.utils.log import get_task_logger
+from celery import current_app
 from django.db.models.signals import post_save
-import requests
-
-
-LOG = get_task_logger(__name__)
-
-
-@shared_task(bind=True)
-def webhook(self, url, body_data):
-    response = requests.patch(url, body_data,
-                              headers={'Content-Type': 'application/json'})
-
-    if int(response.status_code / 200) != 1:
-        LOG.warn('Failed to deliver callback to %s', url)
-        self.retry()
 
 
 def trigger_webhooks(instance, created, **kwargs):
     if instance.subscribedURL:
-        webhook.delay(instance.subscribedURL, {
+        current_app.tasks['webhook'].delay(instance.subscribedURL, {
             'outputs': instance.inputs,
             'status': instance.status,
         })
